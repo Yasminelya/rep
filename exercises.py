e4fba8576b22401f8dd694bed5ce60c2de42ca20
Yasmine <yasmine.lyagoubi@student.ie.edu>
2018-10-26 14:48:55 +0200

    changes in linear ex

--- exercises.py
+++ exercises.py
@@ -6,8 +6,8 @@ Created on Fri Oct 26 14:35:18 2018
 @author: yasminelyagoubi
 """
 
-def linear(lst,number):
+def linear(lst,num):
     for i in range(len(lst)):
-        if lst[i]==number:
+        if lst[i]==num:
             return i
     return None 
